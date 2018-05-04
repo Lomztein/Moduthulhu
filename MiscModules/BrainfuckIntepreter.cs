c114dd61732afbfd51f7fc4daff8577cb80d8836
Lomztein <lomztein2@gmail.com>
2018-05-05 00:30:52 +0200

    Brainfuck is not a thing you want in your life

---
+++ MiscModules/BrainfuckIntepreter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lomztein.ModularDiscordBot.Modules.Misc.Brainfuck
+{
+    public class BrainfuckIntepreter {
+
+        public BrainfuckIntepreter (Func<Task<byte>> _getInput) {
+            getInput = _getInput;
+        }
+
+        public string program;
+        public string result;
+
+        private int counter = 0;
+        private int pointer = 0;
+
+        private Byte [ ] memory = new byte [ 30000 ];
+        public Func<Task<byte>> getInput;
+
+        public async Task<string> Interpret(string program) {
+
+            string printout = string.Empty;
+
+            try {
+                for (counter = 0; counter < program.Length; counter++) {
+                    switch (program [ counter ]) {
+                        case '>':
+                            MovePointer (1);
+                            break;
+
+                        case '<':
+                            MovePointer (-1);
+                            break;
+
+                        case '+':
+                            ChangeMemoryAtPointer (1);
+                            break;
+
+                        case '-':
+                            ChangeMemoryAtPointer (-1);
+                            break;
+
+                        case '[':
+                        case ']':
+                            counter = LoopCounter (counter);
+                            break;
+
+                        case '.':
+                            Printout (ref printout);
+                            break;
+
+                        case ',':
+                            SetMemoryAtPointer (await getInput ());
+                            break;
+                    }
+                }
+            }catch (Exception e) {
+                return e.Message;
+            }
+
+            result = printout;
+            return printout;
+        }
+
+        // The > and < chars, changes memory pointer.
+        private void MovePointer(int movement) {
+            pointer += movement;
+        }
+
+        // The + and - chars, changes memory at pointer.
+        private void ChangeMemoryAtPointer(int movement) {
+            int curAsInt = memory [ pointer ];
+            memory [ pointer ] = (byte)(curAsInt + movement);
+        }
+
+        // The , char, not sure how this is used.
+        private void SetMemoryAtPointer (byte value) {
+            memory [ pointer ] = value;
+        }
+
+        // The . char, prints out bytes.
+        private void Printout(ref string printout) {
+            printout += (char)memory[pointer];
+        }
+
+        // The [ and ] chars, functions as loops/pathers.
+        private int LoopCounter(int position) {
+
+            char starting = program [ position ];
+            int movement = BracketToMovement (program [ position]);
+            int balance = movement;
+
+            int fit = position;
+
+            while (true) {
+                position += movement;
+                balance += BracketToMovement (program [ position]);
+
+                if (IsBracket (program[position])) {
+                    bool isOpposite = program [ position ] == OppositeBracket (starting);
+
+                    // Move forward if zero.
+                    if (starting == '[' && isOpposite && balance == 0) {
+                        if (memory [ pointer ] == 0)
+                            fit = position;
+                        break;
+                    }
+
+                    // Move backwards if non-zero.
+                    if (starting == ']' && isOpposite && balance == 0) {
+                        if (memory [ pointer ] != 0)
+                            fit = position;
+                        break;
+                    }
+                }
+            }
+
+            return fit;
+
+            int BracketToMovement(char bracket) => bracket == '[' ? 1 : bracket == ']' ? -1 : 0;
+            bool IsBracket(char bracket) => bracket == '[' || bracket == ']';
+            int OppositeBracket (char bracket) => bracket == '[' ? ']' : '[';
+
+        }
+    }
+}
---
+++ MiscModules/BrainfuckModule.cs
@@ -0,0 +1,66 @@
+﻿using Lomztein.AdvDiscordCommands.Framework;
+using Lomztein.ModularDiscordBot.Core.Module.Framework;
+using Lomztein.ModularDiscordBot.Modules.CommandRoot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lomztein.ModularDiscordBot.Modules.Misc.Brainfuck
+{
+    public class BrainfuckModule : ModuleBase {
+
+        public override string Name => "Brainfuck";
+        public override string Description => "why does this exist";
+        public override string Author => "I don't want my name on this.";
+
+        public override bool Multiserver => true;
+
+        private BrainfuckCommand command = new BrainfuckCommand ();
+
+        private Dictionary<ulong, char?> input = new Dictionary<ulong, char?> ();
+
+        public override void Initialize() {
+            throw new NotImplementedException ();
+        }
+
+        public override void Shutdown() {
+            throw new NotImplementedException ();
+        }
+
+        private async Task<string> Run (string program, ulong channelID) {
+            if (input.ContainsKey (channelID))
+                throw new Exception ("A Brainfuck program is already running in this channel.");
+
+            input.Add (channelID, null);
+            BrainfuckIntepreter intepreter = new BrainfuckIntepreter (new Func<Task<byte>> (async () => await AwaitInputAsync (channelID)));
+            return await intepreter.Interpret (program);
+        }
+
+        private async Task<byte> AwaitInputAsync(ulong channelID) {
+            if (input.ContainsKey (channelID)) {
+                while (input [ channelID ] == null) {
+                    await Task.Delay (1000);
+                }
+
+                return (byte)input [ channelID ];
+            }
+            throw new ArgumentException ("There is no current program in channel " + channelID + ".");
+        }
+
+        public class BrainfuckCommand : ModuleCommand<BrainfuckModule> {
+
+            public BrainfuckCommand () {
+                command = "brainfuck";
+                shortHelp = "Brainfuck.";
+                catagory = Category.Fun;
+            }
+
+            public async Task<Result> Execute (CommandMetadata metadata, string program) {
+                string result = await parentModule.Run (program, metadata.message.Channel.Id);
+                return new Result (result, result);
+            }
+
+        }
+    }
+}
